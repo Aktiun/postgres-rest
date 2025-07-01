7e0f9d863bbad9ffacd35d15adae01e785f9e463
Jorge Alarcon <jorge@aktiun.com>
2025-07-01 10:31:53 -0400

    Dockerizing

---
+++ local-server/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  host:     process.env.PGHOST     || '127.0.0.1"',
+  port:     process.env.PGPORT     || 5432,
+  user:     process.env.PGUSER     || 'postgres',
+  password: process.env.PGPASSWORD || 'passpg',
+  database: process.env.PGDATABASE || 'postgres'
+}

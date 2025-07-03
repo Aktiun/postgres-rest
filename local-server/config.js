a2d0ec5a336a16802a8293cd1ee768d7f194b987
Jorge Alarcon <jorge@aktiun.com>
2025-07-03 08:16:37 -0400

    Fixing config.js typo

--- local-server/config.js
+++ local-server/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  host:     process.env.PGHOST     || '127.0.0.1"',
+  host:     process.env.PGHOST     || '127.0.0.1',
   port:     process.env.PGPORT     || 5432,
   user:     process.env.PGUSER     || 'postgres',
   password: process.env.PGPASSWORD || 'passpg',
